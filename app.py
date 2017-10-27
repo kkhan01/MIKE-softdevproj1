6dbf55c5a13c6c9f9725d976b1a0fab79ec8a177
Hyhyme <eluo1@stuy.edu>
2017-10-27 11:44:18 -0400

    added to app.y

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 from flask import Flask, session, render_template, request, redirect
-import os
+import os, db_manager.py
 
 Story = Flask(__name__)
 Story.secret_key = os.urandom(64)
-#login.secret_key = os.urandom(64)
 
+def loggedIn(input_user, input_pass):
+        if db_manager.user_exist(input_user):
+                if db_manager.user_pass(input_user) == input_pass:
+                        session['username'] = input_user
+                        return True
+        return False
 
 @Story.route('/')
 def root():
-	return render_template("home.html")
+        if loggedIn():
+                return render_template("home.html")
+        else:
+                return render_template("login.html")
 
 
 @Story.route('/login')
@@ -19,8 +27,6 @@ def login():
 @Story.route('/edit', methods = ['GET', 'POST'])
 def edit():
         return render_template("edit.html")
-
-
 
 if __name__ == "__main__":
     Story.debug = True
