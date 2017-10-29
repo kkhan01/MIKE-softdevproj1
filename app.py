adab56d2be364dd2522a45502f49a1226a953e86
imahdi2000 <imahdi@stuy.edu>
2017-10-29 12:08:19 -0400

    make an account fx and page

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask, session, render_template, request, redirect, flash,  ur
 import os
 from utils import db_manager
 
-Story = Flask(__name__)
-Story.secret_key = os.urandom(64)
+app = Flask(__name__)
+app.secret_key = os.urandom(64)
 
-@Story.route('/', methods = ['GET','POST'])
+@app.route('/', methods = ['GET','POST'])
 def root():
         if 'username' in session:
             return render_template("home.html")
@@ -23,20 +23,20 @@ def root():
         #elif
         return render_template('login.html')
 
-@Story.route('/logout')
+@app.route('/logout')
 def logout():
     if 'username' in session:
         session.pop("username");
     return redirect( url_for('root') )
 
-@Story.route('/edit')
+@app.route('/edit')
 def edit():
     if 'username' in session:
         return render_template("edit.html")
     else:
         return redirect( url_for('root') )
 
-@Story.route('/magic', methods = ["POST"])
+@app.route('/magic', methods = ["POST"])
 def magic():
     if 'username' in session:
         new_edit = request.form["story_edit"]
@@ -44,11 +44,37 @@ def magic():
     else:
         return redirect( url_for('root') )
 
-@Story.route('/create')
+@app.route('/create')
 def create():
     return render_template("create.html")
 
 
+@app.route('/creator', methods = ["POST"])
+def creator():
+    if request.form["username"].strip() == "":
+        flash("No username - No Spaces Please")
+        return render_template('create.html')
+    elif request.form["username"].strip() == "username":
+        flash("username exist")
+        return render_template('create.html')
+    elif request.form["password"].strip() == "":
+        flash("No password - No Spaces Please")
+        return render_template('create.html')
+    elif request.form["npassword"].strip() == "":
+        flash("Re-type password - No Spaces Please")
+        return render_template('create.html')
+    elif request.form["username"].strip() != request.form["npassword"]:
+        flash("Password Don't Match, Try Again")
+        return render_template('create.html')
+    elif (request.form["password"] == request.form["npassword"]) and request.form["password"].strip() != "":
+        new_user = request.form["username"]
+        password = request.form["password"]
+        return render_template('login.html', msg = "Account Made")
+    else:
+        flash("Somthing Went Wrong, Try Again")
+        return render_template('create.html')
+
+
 if __name__ == "__main__":
-    Story.debug = True
-    Story.run()
+    app.debug = True
+    app.run()
